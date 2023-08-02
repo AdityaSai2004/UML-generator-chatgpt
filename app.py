67b178fff01147e422187862d9fa2b16d3dbb4c5
AdityaSai2004 <2004sai@gmail.com>
2023-08-02 13:00:14 +0530

    user api key

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ import base64
 import openai
 import plantuml
 from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+
+# if not os.getenv("OPENAI_API_KEY"):
+#     # Prompt the user for their OpenAI API key if it is not set in the environment variables
+#     api_key = input("Please enter your OpenAI API key: ")
+#     with open(".env", "a") as f:
+#         f.write(f"OPENAI_API_KEY={api_key}\n")
+
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
+        openai_api_key = request.form["openai_api_key"]
+        openai.api_key = openai_api_key
+
+
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
